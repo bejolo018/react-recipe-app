b0e4ac67e6490716cd04a465b9be6a518404c565
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-08 18:13:21 -0500

    displayed ingredients, styled everything

--- recipe-book/src/components/Recipe.js
+++ recipe-book/src/components/Recipe.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import style from './recipe.module.css';
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li key={title}>{ingredient.text}</li>))
+                }
+            </ol>
             <p>{calories} calories</p>
             <img src={image} alt="" />
         </div>
