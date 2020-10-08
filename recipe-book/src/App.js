024ab1f84fd3b063af681faf354fed60d0679055
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-08 18:01:25 -0500

    implemented search functionality and moved get recipes so that warning goes away

--- recipe-book/src/App.js
+++ recipe-book/src/App.js
@@ -6,25 +6,35 @@ const App = () => {
 
   const APP_ID ='697c2c87'
   const APP_KEY = '26985e81fbdff8fdd13ae40f7a0e1601'
-  const request = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   const [recipes, setRecipes] = useState([])
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+      const data = await response.json()
+      setRecipes(data.hits)
+      console.log(data.hits)
+    }
     getRecipes()
-  },[])
+  },[query])
 
-  const getRecipes = async () => {
-    const response = await fetch(request)
-    const data = await response.json()
-    setRecipes(data.hits)
-    console.log(data.hits)
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
   }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" />
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit"> Search </button>
       </form>
       {recipes.map(recipe =>(
